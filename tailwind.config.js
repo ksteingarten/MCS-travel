3e5f92c03e5e20f309a452e1dc8ba507643fc5f5
Kai Steingarten <85062889+ksteingarten@users.noreply.github.com>
2024-11-11 20:48:20 -0800

    Create basic set up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// theme customization here for autocomplete
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            sage: "#95bc81",
+            "dark-sage": "#68845a",
+            "light-sage": "#f2ffeb",
+            purple: "#bc81a1",
+            "dark-purple": "#845a71",
+        },
+    },
+};
